79550762b4dc5cd27469cbb354e19e46077c46ec
Keogh Jokhan <trainee.keogh.jokhan@sigmalabs.co.uk>
2024-12-02 16:49:20 +0000

    Current extract scripts

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,18 +1,60 @@
-from atproto import FirehoseSubscribeLabelsClient
-import asyncio
+from atproto_firehose import FirehoseSubscribeReposClient, parse_subscribe_repos_message
+import ssl
+import certifi
+from atproto import CAR, models
+from atproto_client.models.utils import get_or_create
+import json
 
 
-async def firehose_listener():
-    # Initialize the client
-    client = FirehoseSubscribeLabelsClient()
+class JSONExtra(json.JSONEncoder):
+    """Serializes raw objects (including CID-Content Identifier) as strings."""
 
-    # Log in with your credentials (replace with your actual credentials)
-    # client.login(identifier="your_username_or_email", password="your_password")
+    def default(self, obj: bytes):
+        try:
+            result = json.JSONEncoder.default(self, obj)
+            return result
+        except:
+            return repr(obj)
 
-    # Open the firehose connection
-    async for message in client.firehose():
-        # Handle each message from the firehose
-        print(message)
 
-# Run the listener
-asyncio.run(firehose_listener())
+def get_firehose_data(message) -> list[str]:
+    """"Function handles incoming messages from the BlueSky firehose, parses the raw data 
+    into json objects and returns the message posted."""
+    topics = ['building', 'lake']
+
+    commit = parse_subscribe_repos_message(message)
+    if not isinstance(commit, models.ComAtprotoSyncSubscribeRepos.Commit):
+        return
+    car = CAR.from_bytes(commit.blocks)
+    for op in commit.ops:
+        if op.action in ["create"] and op.cid:
+            raw = car.blocks.get(op.cid)
+            cooked = get_or_create(raw, strict=False)
+            if cooked.py_type == "app.bsky.feed.post":
+
+                json_data = json.dumps(raw, cls=JSONExtra, indent=2)
+                parsed_json = json.loads(json_data)
+                firehose_text = parsed_json['text']
+                if any(ext in firehose_text for ext in topics):
+                    print(firehose_text)
+                    print("="*100)
+
+
+def bluesky_firehose_connection() -> None:
+    """Function to connect to BlueSky Firehose API using a secure SSL context to ensure
+    secure communication."""
+    # Create an SSL context that skips verification
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+    client = FirehoseSubscribeReposClient()
+
+    # Modify the client connection to use this unverified context
+    # Assuming the library allows this (check docs)
+    client.ssl_context = ssl_context
+
+    client.start(get_firehose_data)
+
+
+if __name__ == "__main__":
+    topics = ['blue', 'red', 'chocolate']
+    bluesky_firehose_connection()
