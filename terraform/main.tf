bf0d6d55d5c640f5808599b4c2e59ded2cd3de17
kurt <trainee.kurt.martin-brown@sigmalabs.co.uk>
2024-12-02 16:38:04 +0000

    created the rds database using terraform

---
+++ terraform/main.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+    region = "eu-west-2"
+}
+
+resource "aws_security_group" "rds_sg"{
+    name = "c14-trend-getter-rds-sg"
+    description = "Security group for trend getter RDS database."
+    vpc_id = var.vpc_id
+
+  ingress {
+    description      = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] 
+  }
+
+  ingress {
+    description      = "Allow"
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+} 
+
+
+resource "aws_db_instance" "trend_getter_db" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  instance_class       = var.db_instance_class
+  db_name              = var.db_name
+  username             = var.db_username
+  password             = var.db_password
+  publicly_accessible  = true
+  skip_final_snapshot  = true
+  storage_type         = "gp2"
+  db_subnet_group_name   = "c14-public-subnet-group"
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  identifier = "c14-trend-getter-db"
+
+  deletion_protection = false
+
+  multi_az             = false
+  backup_retention_period = 7
+}
+
+
+
---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "db_name" {
+  description = "The name of the database to create"
+  type        = string
+  default     = "trends"
+}
+
+variable "vpc_id" {
+  description = "The VPC id we are using"
+  type        = string
+  sensitive = true
+}
+
+variable "db_username" {
+  description = "The database username"
+  type        = string
+  sensitive = true
+}
+
+variable "db_password" {
+  description = "The database password"
+  type        = string
+  sensitive = true
+}
+
+variable "db_port" {
+  description = "The database password"
+  type        = string
+  sensitive = true
+}
+
+variable "db_instance_class" {
+  description = "The instance type for the RDS instance"
+  type        = string
+  default     = "db.t3.micro"
+}
