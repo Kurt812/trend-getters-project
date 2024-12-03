74f70c9fbbcdfba1ef38220ee3b0bd3105dd4e19
SurinaCS <surinasanthokhy@yahoo.co.uk>
2024-12-03 10:54:34 +0000

    adding initial file

--- pipeline/test_bluesky_extract.py
+++ pipeline/test_bluesky_extract.py
@@ -2,11 +2,3 @@
 
 
 import pytest
-
-
-def test_extract_botanist_information():
-    """Tests extracts botanists information successfully """
-    input_data = {"name": "Test Test",
-                  "email": "test@test.com", "phone": "+0000 111222"}
-    output_data = {"name": "Test Test",
-                   "email": "test@test.com", "phone": "+0000 111222"}
