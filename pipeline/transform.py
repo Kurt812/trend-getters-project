11912a4d2825ed24dce306a5ca95e8d331aa00dc
Keogh Jokhan <trainee.keogh.jokhan@sigmalabs.co.uk>
2024-12-03 11:43:56 +0000

    Transform changes

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -38,18 +38,18 @@ def get_cursor(config):
 
 def clean_data(bluesky_data: pd.DataFrame, keywords: list[str]) -> pd.DataFrame:
     """Removes any rows that don't include the keywords and removes duplicate rows."""
-    return bluesky_data, keywords
-    # filtered_bluesky = bluesky_data[bluesky_data['content'].str.contains(
-    #     r'\b(?:' + '|'.join(keywords) + r')\b', case=False, na=False, regex=True)]
 
-    # filtered_bluesky = filtered_bluesky.drop_duplicates()
+    # filtered_bluesky = bluesky_data[bluesky_data['text'].str.contains(
+    #    r'\b(?:' + '|'.join(keywords) + r')\b', case=False, na=False, regex=True)]
+    filtered_bluesky = bluesky_data[bluesky_data['keyword'].notnull()]
+    filtered_bluesky = bluesky_data.drop_duplicates()
 
-    # return filtered_bluesky
+    return filtered_bluesky
 
 
 def ensure_keywords_in_db(keywords: list, cursor, connection):
     """Ensure all keywords are present in the database. Add missing keywords."""
-
+    cursor.execute("SET search_path TO trendgineers;")
     cursor.execute("SELECT keyword, keywords_id FROM keywords")
     rows = cursor.fetchall()
 
@@ -60,7 +60,7 @@ def ensure_keywords_in_db(keywords: list, cursor, connection):
         keyword_lower = keyword.lower()  # Normalize for case-insensitivity
         if keyword_lower not in keyword_map:
             cursor.execute(
-                "INSERT INTO keywords (keyword) VALUES (%s)",
+                "INSERT INTO keywords (keyword) VALUES (%s) RETURNING keywords_id",
                 (keyword_lower,)
             )
             connection.commit()
@@ -78,7 +78,7 @@ def keyword_matching(cleaned_bluesky_data: pd.DataFrame, keyword_map: dict) -> p
 
     # Loop through the keywords and assign keyword_id to matching rows
     for keyword, keyword_id in keyword_map.items():
-        mask = cleaned_bluesky_data['content'].str.contains(
+        mask = cleaned_bluesky_data['keyword'].str.contains(
             rf'\b{keyword}\b', case=False, na=False)
         cleaned_bluesky_data.loc[mask, 'keyword_id'] = keyword_id
 
@@ -89,7 +89,7 @@ def add_sentiment_scores(df: pd.DataFrame) -> pd.DataFrame:
     """Find and add the sentiment scores of each message."""
     analyzer = SentimentIntensityAnalyzer()
 
-    df['sentiment_score'] = df['content'].apply(
+    df['sentiment_score'] = df['text'].apply(
         lambda text: analyzer.polarity_scores(text)['compound'])
 
     return df
@@ -116,10 +116,9 @@ if __name__ == "__main__":
     keywords = extract_keywords_from_csv(
         "bluesky_output_data/bluesky_output_20241203_110911.csv")
 
-    print(clean_data(content_dataframe, keywords))
-    # cleaned_dataframe = clean_data(content_dataframe, keywords)
-    # keyword_map = ensure_keywords_in_db(keywords, cursor, conn)
-    # matched_dataframe = keyword_matching(cleaned_dataframe, keyword_map)
-    # final_dataframe = add_sentiment_scores(matched_dataframe)
+    cleaned_dataframe = clean_data(content_dataframe, keywords)
+    keyword_map = ensure_keywords_in_db(keywords, cursor, conn)
+    matched_dataframe = keyword_matching(cleaned_dataframe, keyword_map)
+    final_dataframe = add_sentiment_scores(matched_dataframe)
 
-    # print(final_dataframe)
+    print(final_dataframe)
