651fa22b4bedd2f672feb260036f259697d9c4a1
RidwanHamid501 <ridwanhamid501@gmail.com>
2024-12-04 17:08:21 +0000

    Update schema

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,11 +1,12 @@
+from os import environ as ENV
 import psycopg2
 import psycopg2.extras
-from os import environ as ENV
 from dotenv import load_dotenv
+from extract import main as downloader
+from transform import main as optimus_prime
 
 
-def import_kiosk_data_into_database(keyword: str):
-    """Importing the data into the database"""
+def setup_connection():
     conn = psycopg2.connect(
         user=ENV["DB_USERNAME"],
         password=ENV["DB_PASSWORD"],
@@ -14,19 +15,78 @@ def import_kiosk_data_into_database(keyword: str):
         database=ENV["DB_NAME"]
     )
     cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cursor.execute(f"SET SEARCH_PATH TO {ENV["SCHEMA_NAME"]};")
+    cursor.execute(f"SET SEARCH_PATH TO {ENV['SCHEMA_NAME']};")
+    return conn, cursor
+
+
+def insert_topics(conn, cursor, topics: list[str]) -> list[int]:
+    """Insert user given topics into the topic table"""
+    topic_name = ",".join(sorted(topics))
     cursor.execute(
-        f"""INSERT INTO topic (topic_name)
-        VALUES ('{keyword}')""")
-    conn.commit()
+        "SELECT topic_id FROM topic WHERE topic_name = %s", (topic_name,))
+    existing_id = cursor.fetchone()
+
+    if existing_id is None:
+        cursor.execute("""INSERT INTO topic (topic_name)
+                        VALUES (%s) RETURNING topic_id""", (topic_name,))
+        conn.commit()
+        new_topic_id = cursor.fetchone()['topic_id']
+        return new_topic_id, True
+
+    return existing_id['topic_id'], False
 
 
-def main(keyword: str):
+def insert_keywords(conn, cursor, topics: list[str]) -> list[int]:
+    """Insert keywords into keywordw tabl from topice"""
+    keyword_ids = []
+    for keyword in topics:
+        cursor.execute(
+            "SELECT keywords_id FROM keywords WHERE keyword = %s", (keyword,))
+        existing_id = cursor.fetchone()
 
+        if existing_id is None:
+            cursor.execute("""INSERT INTO keywords (keyword)
+                            VALUES (%s) RETURNING keywords_id""", (keyword,))
+            conn.commit()
+            new_keyword_id = cursor.fetchone()['keywords_id']
+            keyword_ids.append(new_keyword_id)
+            continue
+
+        keyword_ids.append(existing_id['keywords_id'])
+
+    return keyword_ids
+
+
+def assign_keywords(conn, cursor, topic_id: int, keyword_ids: int, is_new_topic: bool) -> None:
+    """Assign keywords"""
+    if is_new_topic:
+        for id in keyword_ids:
+            cursor.execute("""INSERT INTO keyword_assignment (topic_id, keywords_id)
+                                VALUES (%s, %s)""", (topic_id, id))
+        conn.commit()
+
+
+def insert_trend_data():
+    """"""
+    ...
+
+
+def main(topics: list[str]):
+    conn, cursor = setup_connection()
     load_dotenv()
-    import_kiosk_data_into_database(keyword)
+    topic_id, is_new_topic = insert_topics(conn, cursor, topics)
+    print(topic_id)
+    keyword_ids = insert_keywords(conn, cursor, topics)
+    print(keyword_ids)
+    assign_keywords(conn, cursor, topic_id, keyword_ids, is_new_topic)
 
 
 if __name__ == "__main__":
-
-    main('book')
+    topics = ['strawberries', 'chocolate']  # this
+    main(topics)
+    extracted_dataframe = downloader(topics)
+    final_datafram = optimus_prime(extracted_dataframe)
+    final_datafram.to_csv("output.csv", index=False)
+    # unique_names = final_datafram["keyword_id"].unique()
+    # print(unique_names)
+    # main('book')
--- pipeline/schema.sql
+++ pipeline/schema.sql
@@ -1,13 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS :schema_name;
 SET search_path TO :schema_name;
 
-DROP TABLE IF EXISTS trend_data;
-DROP TABLE IF EXISTS keyword_assignment;
-DROP TABLE IF EXISTS keywords;
 DROP TABLE IF EXISTS subscription;
-DROP TABLE IF EXISTS topic;
+DROP TABLE IF EXISTS related_term_assignment;
+DROP TABLE IF EXISTS related_terms;
+DROP TABLE IF EXISTS keyword_recordings;
+DROP TABLE IF EXISTS keywords;
 DROP TABLE IF EXISTS "user";
-DROP TABLE IF EXISTS data_source;
 
 
 SET DateStyle TO 'European'; 
@@ -19,30 +18,6 @@ CREATE TABLE IF NOT EXISTS "user" (
     PRIMARY KEY (user_id)
 );
 
-CREATE TABLE IF NOT EXISTS topic (
-    topic_id INT GENERATED ALWAYS AS IDENTITY,
-    topic_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (topic_id)
-);
-
-CREATE TABLE IF NOT EXISTS data_source (
-    data_source_id SMALLINT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(255) NOT NULL,
-    website_url VARCHAR(255) NOT NULL,
-    PRIMARY KEY (data_source_id)
-);
-
-CREATE TABLE IF NOT EXISTS subscription (
-    subscription_id INT GENERATED ALWAYS AS IDENTITY,
-    user_id INT NOT NULL,
-    topic_id INT NOT NULL,
-    subscription_status BOOLEAN NOT NULL,
-    notification_threshold SMALLINT,
-    PRIMARY KEY (subscription_id),
-    FOREIGN KEY (user_id) REFERENCES "user"(user_id),
-    FOREIGN KEY (topic_id) REFERENCES topic(topic_id)
-);
-
 
 CREATE TABLE IF NOT EXISTS keywords (
     keywords_id BIGINT GENERATED ALWAYS AS IDENTITY,
@@ -50,30 +25,51 @@ CREATE TABLE IF NOT EXISTS keywords (
     PRIMARY KEY (keywords_id)
 ); 
 
-CREATE TABLE IF NOT EXISTS keyword_assignment (
-    keyword_assignment_id BIGINT GENERATED ALWAYS AS IDENTITY,
-    topic_id BIGINT,
+
+CREATE TABLE IF NOT EXISTS keyword_recordings (
+    keyword_recordings_id BIGINT GENERATED ALWAYS AS IDENTITY,
     keywords_id BIGINT,
-    PRIMARY KEY (keyword_assignment_id),
-    FOREIGN KEY (topic_id) REFERENCES topic(topic_id),
+    total_mentions SMALLINT,
+    avg_sentiment SMALLINT,
+    hour_of_day SMALLINT,
+    PRIMARY KEY (keyword_recordings_id),
     FOREIGN KEY (keywords_id) REFERENCES keywords(keywords_id)
 );
 
 
-CREATE TABLE IF NOT EXISTS trend_data (
-    trend_data_id BIGINT GENERATED ALWAYS AS IDENTITY,
-    topic_id INT,
-    data_source_id SMALLINT,
-    time_recorded TIMESTAMPTZ NOT NULL,
-    sentiment_score FLOAT NOT NULL,
-    mentions_count SMALLINT NOT NULL,
-    related_terms VARCHAR(255) NOT NULL,
-    PRIMARY KEY (trend_data_id),
-    FOREIGN KEY (topic_id) REFERENCES topic(topic_id),
-    FOREIGN KEY (data_source_id) REFERENCES data_source(data_source_id)
+CREATE TABLE IF NOT EXISTS related_terms (
+    related_term_id BIGINT GENERATED ALWAYS AS IDENTITY,
+    related_term VARCHAR(255) NOT NULL,
+    PRIMARY KEY (related_term_id),
+    FOREIGN KEY (related_term_id) REFERENCES related_terms(related_term_id)
 );
 
--- Seed data source Data
-INSERT INTO data_source (name, website_url) VALUES 
-('BlueSky Firehose', 'https://bsky.social/'),
-('Google Trends', 'https://trends.google.com/trends/')
+
+CREATE TABLE IF NOT EXISTS related_term_assignment (
+    related_term_assignment BIGINT GENERATED ALWAYS AS IDENTITY,
+    keywords_id BIGINT,
+    related_term_id BIGINT,
+    PRIMARY KEY (related_term_assignment),
+    FOREIGN KEY (keywords_id) REFERENCES keywords(keywords_id),
+    FOREIGN KEY (related_term_id) REFERENCES related_terms(related_term_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS subscription (
+    subscription_id INT GENERATED ALWAYS AS IDENTITY,
+    user_id INT NOT NULL,
+    keywords_id INT NOT NULL,
+    subscription_status BOOLEAN NOT NULL,
+    notification_threshold SMALLINT,
+    PRIMARY KEY (subscription_id),
+    FOREIGN KEY (user_id) REFERENCES "user"(user_id),
+    FOREIGN KEY (keywords_id) REFERENCES keywords(keywords_id)
+);
+
+
+
+
+
+
+
+
