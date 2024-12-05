2ed6a8ab026d11e549ecdb5b2f831b025ef88cc4
Keogh Jokhan <trainee.keogh.jokhan@sigmalabs.co.uk>
2024-12-05 15:24:16 +0000

    pylint changes

--- pipeline/load.py
+++ pipeline/load.py
@@ -4,10 +4,8 @@ from os import environ as ENV
 import pandas as pd
 import psycopg2
 import psycopg2.extras
-from psycopg2.extensions import connection, cursor
+from psycopg2.extensions import connection as connect, cursor as curs
 from dotenv import load_dotenv
-from extract import main as extract_main
-from transform import main as transform_main
 
 
 def setup_connection():
@@ -24,10 +22,10 @@ def setup_connection():
     return conn, cursor
 
 
-def insert_keywords(conn: connection, cursor: cursor, 
-                    topics: list[str]) -> None:
+def insert_keywords(conn: connect, cursor: curs,
+                    topic: list[str]) -> None:
     """Insert keywords into keywords table from topic"""
-    for keyword in topics:
+    for keyword in topic:
         cursor.execute(
             "SELECT keywords_id FROM keywords WHERE keyword = %s", (keyword,))
         existing_id = cursor.fetchone()
@@ -38,8 +36,8 @@ def insert_keywords(conn: connection, cursor: cursor,
             conn.commit()
 
 
-def insert_keyword_recordings(conn: connection, 
-                              cursor: cursor, dataframe: pd.DataFrame) -> None:
+def insert_keyword_recordings(conn: connect,
+                              cursor: curs, dataframe: pd.DataFrame) -> None:
     """Inserts data into the keyword_recordings table"""
 
     for row in dataframe.to_dict(orient='records'):
@@ -47,26 +45,28 @@ def insert_keyword_recordings(conn: connection,
         total_mentions = row['Count']
         average_sentiment = row['Average Sentiment']
         keyword_id = row['keyword_id']
-        cursor.execute("""INSERT INTO keyword_recordings 
+        cursor.execute("""INSERT INTO keyword_recordings
                        (keywords_id, total_mentions, avg_sentiment, hour_of_day)
-                       VALUES (%s, %s, %s, %s)""", (keyword_id, total_mentions, average_sentiment, hour))
+                       VALUES (%s, %s, %s, %s)""",
+                       (keyword_id, total_mentions, average_sentiment, hour))
         conn.commit()
 
 
-def insert_related_terms(conn: connection, cursor: cursor):
+def insert_related_terms(conn: connect, cursor: curs) -> dict:
     """Inserts unique related terms into the related terms table"""
     dataframe = pd.read_csv("output.csv")
     keyword_and_ids = {}
-    processed_terms = set()  # Track unique terms
+    processed_terms = set()
 
     for row in dataframe.to_dict(orient='records'):
         related_terms = row['Related Terms'].split(",")
         keyword = row['Keyword']
-        
+
         for term in related_terms:
             term = term.strip()
             if term not in processed_terms:
-                cursor.execute("SELECT related_term_id FROM related_terms WHERE related_term = %s", (term, ))
+                cursor.execute("""SELECT related_term_id FROM related_terms
+                               WHERE related_term = %s""", (term, ))
                 existing_term = cursor.fetchone()
 
                 if existing_term is None:
@@ -83,13 +83,13 @@ def insert_related_terms(conn: connection, cursor: cursor):
     return keyword_and_ids
 
 
-def get_keyword_id(cursor: cursor, keyword: str) -> int:
+def get_keyword_id(cursor: curs, keyword: str) -> int:
     """Returns the keyword id for a given keyword"""
     cursor.execute("""SELECT keywords.keywords_id FROM keywords WHERE keyword = %s""", (keyword, ))
     return cursor.fetchone()['keywords_id']
 
 
-def insert_related_term_assignment(conn: connection, cursor: cursor, keyword_and_ids: dict) -> None:
+def insert_related_term_assignment(conn: connect, cursor: curs, keyword_and_ids: dict) -> None:
     """Inserts data into the related_term_assignment table"""
     for key, value in keyword_and_ids.items():
         keyword_id = get_keyword_id(cursor, value)
@@ -97,21 +97,16 @@ def insert_related_term_assignment(conn: connection, cursor: cursor, keyword_and
                        VALUES (%s, %s)""", (keyword_id, key))
         conn.commit()
 
-def main(topics: list[str], extracted_dataframe: pd.DataFrame):
+
+def main(topic: list[str], extracted_dataframe: pd.DataFrame) -> None:
     """Main function to load environment variables to import data into the database."""
     conn, cursor = setup_connection()
     load_dotenv()
-    insert_keywords(conn, cursor, topics)
+    insert_keywords(conn, cursor, topic)
     insert_keyword_recordings(conn,cursor, extracted_dataframe)
     related_term_ids = insert_related_terms(conn,cursor)
     insert_related_term_assignment(conn,cursor,related_term_ids)
 
 
 if __name__ == "__main__":
-    topics = ['strawberries', 'clouds']  # this
-    main(topics)
-
-    # extracted_dataframe = extract_main(topics)
-    # final_dataframe = transform_main(extracted_dataframe)
-
-    # final_dataframe.to_csv("output.csv", index=False)
+    topics = ['strawberries', 'clouds']
