29e40ab356cb98d5e5c9f8852fd7aed0f3a3e301
kurt <trainee.kurt.martin-brown@sigmalabs.co.uk>
2024-12-06 12:51:40 +0000

    update s3 etl lambdafied

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -58,13 +58,13 @@ variable "db_instance_class" {
   default     = "db.t3.micro"
 }
 
-variable "AWS_ACCESS_KEY_ID" {
+variable "ACCESS_KEY_ID" {
   description = "The AWS access key id"
   type        = string
   sensitive = true
 }
 
-variable "AWS_SECRET_ACCESS_KEY" {
+variable "SECRET_ACCESS_KEY" {
   description = "The AWS secret access key"
   type        = string
   sensitive = true
