c3c372beb05cb7bb2ae3201524a57b499bcd95ac
RidwanHamid501 <ridwanhamid501@gmail.com>
2024-12-09 11:05:45 +0000

    Update api to work with dashboard

--- pipeline/api.py
+++ pipeline/api.py
@@ -1,16 +1,13 @@
 """Api.py: script setting up api to post new topics to."""
 
-import re
 from flask import Flask, request, jsonify
-from load import main
+from etl import main
 
 app = Flask(__name__)
 
-topics = []
-
 
 @app.route("/topics", methods=["POST"])
-def add_topic()-> None:
+def add_topic() -> None:
     """API endpoint to add new topics to RDS."""
     data = request.get_json()
     topic_name = data.get("topic_name")
@@ -18,20 +15,9 @@ def add_topic()-> None:
     if not topic_name:
         return jsonify({"message": "Topic name is required"}), 400
 
-    topic = {
-        "topic_name": topic_name,
-    }
-    topics.append(topic)
-    print([topic['topic_name']])
     main([topic_name])
 
-    return jsonify({"message": "Topic added successfully", "topic": topic}), 200
-
-
-@app.route("/topics", methods=["GET"])
-def get_topics()-> None:
-    """API endpoint to retrieve list of all posted topics."""
-    return jsonify({"topics": topics}), 200
+    return jsonify({"message": "Topic added successfully", "topic": topic_name}), 200
 
 
 if __name__ == "__main__":
